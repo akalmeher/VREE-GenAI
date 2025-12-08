c4af345b3f3c5309b56901c5159004a733a7f3ab
monishamekala <monishasmekala@gmail.com>
2025-12-07 22:14:24 -0800

    layer lab 2 readme, ext, prefabs

---
+++ Assets/Layer Lab/GUI Pro-FantasyRPG/Extensions/UIParticle/UIParticleSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:007a2583de799cf6747cbef588be651e88a86614d258d56439800c2ce95592bd
+size 15386
---
+++ Assets/Layer Lab/GUI Pro-FantasyRPG/Extensions/UIParticle/hidden particle.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a3fcd7f2b5444a3ba7c71f04a6daed4e3fbbc0baae52c8c7dde9be8fda26518
+size 464
